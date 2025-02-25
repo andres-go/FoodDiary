c85727cd0954a6184f8cac849c2bfacc636b1d4f
nicolas-septien <nicoseptiencool@gmail.com>
2025-02-24 18:38:14 -0800

    First commit

---
+++ FoodDiary/FoodDiary/FoodDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDiaryApp.swift
+//  FoodDiary
+//
+//  Created by Nicolas Septien on 24/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FoodDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
